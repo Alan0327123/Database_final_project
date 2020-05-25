a0abce20e3ed127275888724f2a55d530d91b177
Alan0327123 <65814792+Alan0327123@users.noreply.github.com>
2020-05-25 21:59:23 +0800

    Update create_steam_description_data.sql

--- create_steam_description_data.sql
+++ create_steam_description_data.sql
@@ -1,14 +1,14 @@
 create table steam_description_data(
-    steam_appid int,
-    detailed_descripition varchar(10000),
-    about_the_game varchar(10000),
-    short_description varchar(10000),
+    steam_appid int not NULL,
+    detailed_descriptione TEXT,
+    about_the_game TEXT,
+    short_description TEXT,
     primary key (steam_appid)
-);
-
+    );
+    
 load data local infile './steam_description_data.csv'
 into table steam_description_data
 fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
+OPTIONALLY ENCLOSED BY  '\"'
+lines terminated by '\r\n'
 ignore 1 lines;
